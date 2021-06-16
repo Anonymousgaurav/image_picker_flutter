fd3b17017818715f33a0c71357de41ed9fd90cdf
Gaurav <gauravswarankar1@gmail.com>
2021-06-16 10:57:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File _image;
+  ImagePicker imagePicker;
+
+  @override
+  void initState() {
+    super.initState();
+    imagePicker = ImagePicker();
+  }
+
+  Future<void> _chooseGallery() async {
+    PickedFile pickedFile = await imagePicker.getImage(source: ImageSource.gallery);
+    setState(() {
+      _image = File(pickedFile.path);
+    });
+  }
+
+
+
+  Future<void> _chooseCamera() async {
+    PickedFile pickedFile =
+        await imagePicker.getImage(source: ImageSource.camera);
+    setState(() {
+      _image = File(pickedFile.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _image != null ? Image.file(_image) : Icon(Icons.image),
+            MaterialButton(
+                onPressed: _chooseGallery,
+                onLongPress: _chooseCamera,
+                child: Text('choose/capture'))
+          ],
+        ),
+      ),
+    );
+  }
+}
